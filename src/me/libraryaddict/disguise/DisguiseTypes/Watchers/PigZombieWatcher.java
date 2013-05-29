4ffb4a4489d39fcbd57b3e6e8357b5193c457423
Andrew <libraryaddict115@yahoo.co.nz>
2013-05-30 08:11:41 +1200

    Fixed pig zombie crash

--- src/me/libraryaddict/disguise/DisguiseTypes/Watchers/PigZombieWatcher.java
+++ src/me/libraryaddict/disguise/DisguiseTypes/Watchers/PigZombieWatcher.java
@@ -1,9 +1,23 @@
 package me.libraryaddict.disguise.DisguiseTypes.Watchers;
 
-public class PigZombieWatcher extends AgeableWatcher {
+import me.libraryaddict.disguise.DisguiseTypes.FlagWatcher;
+
+public class PigZombieWatcher extends FlagWatcher {
 
     public PigZombieWatcher(int entityId) {
         super(entityId);
+        setValue(12, (byte) 0);
+    }
+
+    public boolean isBaby() {
+        return (Byte) getValue(12) == 1;
+    }
+
+    public void setBaby(boolean baby) {
+        if (isBaby() != baby) {
+            setValue(12, (byte) (baby ? 1 : 0));
+            sendData(12);
+        }
     }
 
 }
