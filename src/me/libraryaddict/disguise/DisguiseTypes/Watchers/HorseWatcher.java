c2530f4fe20b1713c2000a89a2de0db45d8b0a72
Andrew <libraryaddict115@yahoo.co.nz>
2013-07-07 19:18:23 +1200

    Add horse watcher

---
+++ src/me/libraryaddict/disguise/DisguiseTypes/Watchers/HorseWatcher.java
@@ -0,0 +1,14 @@
+package me.libraryaddict.disguise.DisguiseTypes.Watchers;
+
+public class HorseWatcher extends AgeableWatcher {
+
+    public HorseWatcher(int entityId) {
+        super(entityId);
+        setValue(16, 0);
+        setValue(19, (byte) 0);
+        setValue(20, 0);
+        setValue(21, "");
+        setValue(22, 0);
+    }
+
+}
