e0129c154a90bb1d85bf6d6c4b49865246962cc5
Andrew <libraryaddict115@yahoo.co.nz>
2013-07-07 20:38:07 +1200

    Added ability to change horse types

--- src/me/libraryaddict/disguise/DisguiseTypes/Watchers/HorseWatcher.java
+++ src/me/libraryaddict/disguise/DisguiseTypes/Watchers/HorseWatcher.java
@@ -5,10 +5,25 @@ public class HorseWatcher extends AgeableWatcher {
     public HorseWatcher(int entityId) {
         super(entityId);
         setValue(16, 0);
+        // Horse types (19) are
+        // Horse
+        // Donkey
+        // Mule
+        // Zombie
+        // Skeleton
         setValue(19, (byte) 0);
         setValue(20, 0);
         setValue(21, "");
         setValue(22, 0);
+    }
+
+    public void setHorseType(int type) {
+        setValue(19, (byte) type);
+        sendData(19);
+    }
+
+    public int getHorseType() {
+        return (int) (Byte) getValue(19);
     }
 
 }
