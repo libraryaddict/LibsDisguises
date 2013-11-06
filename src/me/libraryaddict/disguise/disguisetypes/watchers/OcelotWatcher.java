86169422a7dffc4ed88f9c902bc027e9094f6b99
Andrew <libraryaddict115@yahoo.co.nz>
2013-11-06 21:02:53 +1300

    SetType(int) added to ocelot

--- src/me/libraryaddict/disguise/disguisetypes/watchers/OcelotWatcher.java
+++ src/me/libraryaddict/disguise/disguisetypes/watchers/OcelotWatcher.java
@@ -1,11 +1,9 @@
 package me.libraryaddict.disguise.disguisetypes.watchers;
 
-
 import me.libraryaddict.disguise.disguisetypes.Disguise;
 
 import org.bukkit.entity.Ocelot;
 import org.bukkit.entity.Ocelot.Type;
-
 
 public class OcelotWatcher extends AgeableWatcher {
 
@@ -38,6 +36,11 @@ public class OcelotWatcher extends AgeableWatcher {
 
     public void setType(Type newType) {
         setValue(18, (byte) newType.getId());
+        sendData(18);
+    }
+
+    public void setType(int type) {
+        setValue(18, (byte) type);
         sendData(18);
     }
 }
