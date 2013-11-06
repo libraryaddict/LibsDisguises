2843022f6c26352b15c225608576ef8cf2086b20
Andrew <libraryaddict115@yahoo.co.nz>
2013-11-06 20:04:32 +1300

    Added itemstack to enderman watcher

--- src/me/libraryaddict/disguise/disguisetypes/watchers/EndermanWatcher.java
+++ src/me/libraryaddict/disguise/disguisetypes/watchers/EndermanWatcher.java
@@ -1,5 +1,7 @@
 package me.libraryaddict.disguise.disguisetypes.watchers;
 
+import org.bukkit.inventory.ItemStack;
+
 import me.libraryaddict.disguise.disguisetypes.Disguise;
 
 public class EndermanWatcher extends LivingWatcher {
@@ -25,11 +27,17 @@ public class EndermanWatcher extends LivingWatcher {
         sendData(18);
     }
 
+    @Deprecated
     public void setCarriedItem(int id, int dataValue) {
         setValue(16, (byte) (id & 255));
         setValue(17, (byte) (dataValue & 255));
         sendData(16);
         sendData(17);
+    }
+
+    public void setCarriedItem(ItemStack itemstack) {
+        setValue(16, (byte) (itemstack.getTypeId() & 255));
+        setValue(17, (byte) (itemstack.getDurability() & 255));
     }
 
 }
