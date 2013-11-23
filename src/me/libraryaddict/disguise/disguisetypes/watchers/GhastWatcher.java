3ab4706af33319bb5acf7af96ac8cdbce1635eae
libraryaddict <libraryaddict115@yahoo.co.nz>
2013-11-24 08:45:32 +1300

    Fixed spelling in GhastWatcher

--- src/me/libraryaddict/disguise/disguisetypes/watchers/GhastWatcher.java
+++ src/me/libraryaddict/disguise/disguisetypes/watchers/GhastWatcher.java
@@ -8,11 +8,22 @@ public class GhastWatcher extends LivingWatcher {
         super(disguise);
     }
 
+    @Deprecated
     public boolean isAgressive() {
         return (Byte) getValue(16, (byte) 0) == 1;
     }
 
+    @Deprecated
     public void setAgressive(boolean isAgressive) {
+        setValue(16, (byte) (isAgressive ? 1 : 0));
+        sendData(16);
+    }
+
+    public boolean isAggressive() {
+        return (Byte) getValue(16, (byte) 0) == 1;
+    }
+
+    public void setAggressive(boolean isAgressive) {
         setValue(16, (byte) (isAgressive ? 1 : 0));
         sendData(16);
     }
