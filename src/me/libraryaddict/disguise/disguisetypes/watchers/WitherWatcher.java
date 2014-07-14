56f3c2394920ad28d9a6f21be1793c6785e2b914
libraryaddict <libraryaddict115@yahoo.co.nz>
2014-07-14 12:25:48 +1200

    Add setTargets to wither watcher

--- src/me/libraryaddict/disguise/disguisetypes/watchers/WitherWatcher.java
+++ src/me/libraryaddict/disguise/disguisetypes/watchers/WitherWatcher.java
@@ -1,5 +1,9 @@
 package me.libraryaddict.disguise.disguisetypes.watchers;
 
+import java.security.InvalidParameterException;
+
+import org.bukkit.ChatColor;
+
 import me.libraryaddict.disguise.disguisetypes.Disguise;
 
 public class WitherWatcher extends LivingWatcher {
@@ -17,12 +21,27 @@ public class WitherWatcher extends LivingWatcher {
     }
 
     public void setInvul(int invulnerability) {
-        setValue(20, invulnerability);
+        setInvulnerability(invulnerability);
     }
 
     public void setInvulnerability(int invulnerability) {
         setValue(20, invulnerability);
         sendData(20);
+    }
+
+    public void setTargets(int... targets) {
+        if (targets.length != 3) {
+            throw new InvalidParameterException(ChatColor.RED + "Expected 3 numbers for wither setTargets. Received "
+                    + targets.length);
+        }
+        setValue(17, targets[0]);
+        setValue(18, targets[1]);
+        setValue(19, targets[2]);
+        sendData(17, 18, 19);
+    }
+
+    public int[] getTargets() {
+        return new int[] { (Integer) getValue(17, 0), (Integer) getValue(18, 0), (Integer) getValue(19, 0) };
     }
 
 }
