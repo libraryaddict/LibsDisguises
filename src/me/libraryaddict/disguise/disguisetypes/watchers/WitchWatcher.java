7d0175c56331cce0e7f68a06fde557b1ae0a6a5d
libraryaddict <libraryaddict115@yahoo.co.nz>
2014-07-14 12:13:30 +1200

    Add witchwatcher (isAggressive and setAggressive)

---
+++ src/me/libraryaddict/disguise/disguisetypes/watchers/WitchWatcher.java
@@ -0,0 +1,20 @@
+package me.libraryaddict.disguise.disguisetypes.watchers;
+
+import me.libraryaddict.disguise.disguisetypes.Disguise;
+
+public class WitchWatcher extends LivingWatcher {
+
+    public WitchWatcher(Disguise disguise) {
+        super(disguise);
+    }
+
+    public void setAggressive(boolean isTrue) {
+        setValue(21, (byte) (isTrue ? 1 : 0));
+        sendData(21);
+    }
+
+    public boolean isAggressive() {
+        return (Byte) getValue(21, (byte) 0) == 1;
+    }
+
+}
