ca9a69f569fd93e01bc903a43b6e97853ed6d58b
libraryaddict <github@lib.co.nz>
2016-05-13 00:56:24 +1200

    Dumb error, I havn't been sleeping well lately.

--- src/me/libraryaddict/disguise/utilities/DisguiseUtilities.java
+++ src/me/libraryaddict/disguise/utilities/DisguiseUtilities.java
@@ -455,7 +455,7 @@ public class DisguiseUtilities
             // Make unload packets
 
             packets[i] = ProtocolLibrary.getProtocolManager().createPacketConstructor(Server.UNLOAD_CHUNK, chunkX, chunkZ)
-                    .createPacket(bedChunk, 0);
+                    .createPacket(bedChunk, chunkX, chunkZ);
 
             i++;
 
