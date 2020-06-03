37bb149e2888f88389590929d2dbf0bbfb2985db
Gust09 <fabiogoncalvesalmeida@gmail.com>
2020-06-04 00:16:15 +0100

    Update DisguiseBaseCommand.java

--- src/main/java/me/libraryaddict/disguise/commands/DisguiseBaseCommand.java
+++ src/main/java/me/libraryaddict/disguise/commands/DisguiseBaseCommand.java
@@ -48,12 +48,13 @@ public abstract class DisguiseBaseCommand implements CommandExecutor {
     }
 
     protected boolean isNotPremium(CommandSender sender) {
+        /*
         if (sender instanceof Player && !sender.isOp() &&
                 (!LibsPremium.isPremium() || LibsPremium.getPaidInformation() == LibsPremium.getPluginInformation())) {
             sender.sendMessage(ChatColor.RED + "Please purchase Lib's Disguises to enable player commands");
             return true;
         }
-
+        */
         return false;
     }
 
