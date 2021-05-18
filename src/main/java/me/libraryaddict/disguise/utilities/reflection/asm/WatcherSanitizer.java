60b79ae789d3af5474e30eb96f182eb58d8b5cf0
libraryaddict <github@lib.co.nz>
2021-05-19 06:05:35 +1200

    Don't throw errors if field doesn't exist

--- src/main/java/me/libraryaddict/disguise/utilities/reflection/asm/WatcherSanitizer.java
+++ src/main/java/me/libraryaddict/disguise/utilities/reflection/asm/WatcherSanitizer.java
@@ -67,7 +67,6 @@ public class WatcherSanitizer {
         try {
             checkPreLoaded();
         } catch (NoSuchFieldException | IllegalAccessException e) {
-            e.printStackTrace();
         }
 
         ArrayList<String> mapped = new ArrayList<>();
