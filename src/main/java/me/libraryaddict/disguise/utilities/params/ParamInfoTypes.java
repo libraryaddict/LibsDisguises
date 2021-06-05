c8783a6efaf04167a90492697c1d0f56798f21f8
paihuai <l89669@hotmail.com>
2021-06-05 22:40:28 +0800

    Fix forge server console spam on plugin loading

--- src/main/java/me/libraryaddict/disguise/utilities/params/ParamInfoTypes.java
+++ src/main/java/me/libraryaddict/disguise/utilities/params/ParamInfoTypes.java
@@ -186,8 +186,7 @@ public class ParamInfoTypes {
                 }
 
                 list.add(material);
-            } catch (NoSuchFieldException e) {
-                e.printStackTrace();
+            } catch (NoSuchFieldException ignored) {
             }
         }
 
