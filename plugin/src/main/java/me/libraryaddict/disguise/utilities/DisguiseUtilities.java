f7b285577dba5b4e3ca85a9a5fb7629b20a18253
libraryaddict <github@lib.co.nz>
2023-04-01 01:06:10 +1300

    Hmm

--- plugin/src/main/java/me/libraryaddict/disguise/utilities/DisguiseUtilities.java
+++ plugin/src/main/java/me/libraryaddict/disguise/utilities/DisguiseUtilities.java
@@ -557,8 +557,13 @@ public class DisguiseUtilities {
             return new String[]{"4.8.0"};
         }
 
+        // If you're on 1.19.0
+        if (NmsVersion.v1_19_R1.isSupported()) {
+            return new String[]{"5.0.1", "600"};
+        }
+
         // If you're on 1.19.1 or 1.19.2
-        if (!NmsVersion.v1_19_R2.isSupported()) {
+        if (NmsVersion.v1_19_R2.isSupported()) {
             return new String[]{"5.0.1", "600"};
         }
 
