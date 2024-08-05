fb876d73449c8c00138b77f8d4f5dc2b69b67c46
libraryaddict <github@lib.co.nz>
2024-08-06 02:05:02 +1200

    .

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -22,18 +22,10 @@ publishing {
         maven {
             name = "md_5-releases"
             url = uri("https://repo.md-5.net/content/repositories/releases/")
-            credentials {
-                username = project.findProperty("repoUser") as String? ?: System.getenv("REPO_USER")
-                password = project.findProperty("repoPassword") as String? ?: System.getenv("REPO_PASSWORD")
-            }
         }
         maven {
             name = "md_5-snapshots"
             url = uri("https://repo.md-5.net/content/repositories/snapshots/")
-            credentials {
-                username = project.findProperty("repoUser") as String? ?: System.getenv("REPO_USER")
-                password = project.findProperty("repoPassword") as String? ?: System.getenv("REPO_PASSWORD")
-            }
         }
         mavenLocal()
     }
