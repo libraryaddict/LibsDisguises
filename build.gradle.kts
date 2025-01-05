0aebcd1ca9808158279a097b0c75ad6b2becbc3e
libraryaddict <github@lib.co.nz>
2025-01-06 00:00:56 +1300

    Fix paper repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ subprojects {
         }
 
         maven {
-            url = uri("https://papermc.io/repo/repository/maven-public/")
+            url = uri("https://repo.papermc.io/repository/maven-public/")
         }
 
         maven {
