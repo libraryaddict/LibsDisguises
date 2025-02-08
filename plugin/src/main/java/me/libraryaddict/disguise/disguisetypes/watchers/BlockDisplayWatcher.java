8a07ccff647c330e1a7e12ade7492c4a047c82c3
libraryaddict <libraryaddict@users.noreply.github.com>
2025-02-08 21:39:38 +1300

    Make block display automatically center when they're scaled

--- plugin/src/main/java/me/libraryaddict/disguise/disguisetypes/watchers/BlockDisplayWatcher.java
+++ plugin/src/main/java/me/libraryaddict/disguise/disguisetypes/watchers/BlockDisplayWatcher.java
@@ -5,19 +5,30 @@ import com.github.retrooper.packetevents.protocol.world.states.type.StateTypes;
 import io.github.retrooper.packetevents.util.SpigotConversionUtil;
 import me.libraryaddict.disguise.disguisetypes.Disguise;
 import me.libraryaddict.disguise.disguisetypes.MetaIndex;
+import me.libraryaddict.disguise.utilities.parser.RandomDefaultValue;
 import me.libraryaddict.disguise.utilities.reflection.annotations.MethodDescription;
 import me.libraryaddict.disguise.utilities.reflection.annotations.MethodMappedAs;
 import org.bukkit.block.data.BlockData;
 import org.joml.Vector3f;
 
 public class BlockDisplayWatcher extends DisplayWatcher {
+    private boolean autoCenter = true;
+
     public BlockDisplayWatcher(Disguise disguise) {
         super(disguise);
 
         // So we're not seeing air
         setBlockState(WrappedBlockState.getDefaultState(StateTypes.STONE));
         // So its centered
-        setTranslation(new Vector3f(-0.5f, 0f, -0.5f));
+        adjustCenter();
+    }
+
+    @Override
+    public BlockDisplayWatcher clone(Disguise owningDisguise) {
+        BlockDisplayWatcher displayWatcher = (BlockDisplayWatcher) super.clone(owningDisguise);
+        displayWatcher.setAutoCentered(autoCenter);
+
+        return displayWatcher;
     }
 
     @MethodMappedAs("getBlock")
@@ -37,5 +48,67 @@ public class BlockDisplayWatcher extends DisplayWatcher {
 
     public void setBlock(BlockData block) {
         sendData(MetaIndex.BLOCK_DISPLAY_BLOCK_STATE, SpigotConversionUtil.fromBukkitBlockData(block));
+    }
+
+    public boolean isAutoCentered() {
+        return autoCenter;
+    }
+
+    public void setAutoCentered(boolean autoCenter) {
+        this.autoCenter = autoCenter;
+
+        adjustCenter();
+    }
+
+    @Override
+    // Because BlockDisplayWatcher modifies this on startup..
+    @RandomDefaultValue
+    public void setTranslation(Vector3f translation) {
+        super.setTranslation(translation);
+
+        // Detect if this is centered or not, if its not centered, we don't want to ruin their settings
+        Vector3f scale = getScale();
+
+        // We always set a 0 y
+        // We always set the X and Z to be half of the scale, negative
+        if (translation.y() == 0 && translation.x() == -scale.x() / 2f && translation.z() == -scale.z() / 2f) {
+            return;
+        }
+
+        setAutoCentered(false);
+    }
+
+    @Override
+    public Vector3f getTranslation() {
+        return super.getTranslation();
+    }
+
+    private void adjustCenter() {
+        if (!isAutoCentered()) {
+            return;
+        }
+
+        Vector3f scale = getScale();
+        Vector3f oldTranslation = getTranslation();
+        Vector3f newTranslation = new Vector3f(-scale.x() / 2f, 0f, -scale.z() / 2f);
+
+        if (oldTranslation.x() == newTranslation.x() && oldTranslation.y() == newTranslation.y() &&
+            oldTranslation.z() == newTranslation.z()) {
+            return;
+        }
+
+        setTranslation(newTranslation);
+    }
+
+    @Override
+    public Vector3f getScale() {
+        return super.getScale();
+    }
+
+    @Override
+    public void setScale(Vector3f scale) {
+        super.setScale(scale);
+
+        adjustCenter();
     }
 }
