4ec23761a99046e19084f0cc043a0c549c6bcf14
libraryaddict <libraryaddict@users.noreply.github.com>
2025-06-22 19:00:13 +1200

    Add random mushroom cow variant

--- plugin/src/main/java/me/libraryaddict/disguise/disguisetypes/watchers/MushroomCowWatcher.java
+++ plugin/src/main/java/me/libraryaddict/disguise/disguisetypes/watchers/MushroomCowWatcher.java
@@ -1,14 +1,20 @@
 package me.libraryaddict.disguise.disguisetypes.watchers;
 
+import me.libraryaddict.disguise.DisguiseConfig;
 import me.libraryaddict.disguise.disguisetypes.Disguise;
 import me.libraryaddict.disguise.disguisetypes.MetaIndex;
 import me.libraryaddict.disguise.utilities.reflection.NmsVersion;
+import me.libraryaddict.disguise.utilities.reflection.ReflectionManager;
 import me.libraryaddict.disguise.utilities.reflection.annotations.NmsAddedIn;
 import org.bukkit.entity.MushroomCow;
 
 public class MushroomCowWatcher extends AgeableWatcher {
     public MushroomCowWatcher(Disguise disguise) {
         super(disguise);
+
+        if (DisguiseConfig.isRandomDisguises()) {
+            setVariant(ReflectionManager.randomEnum(MushroomCow.Variant.class));
+        }
     }
 
     @NmsAddedIn(NmsVersion.v1_14)
