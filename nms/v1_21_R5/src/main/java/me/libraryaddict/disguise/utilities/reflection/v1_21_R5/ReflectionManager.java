b31dc266bcb109f2b2cf540e2138d816808a72ac
libraryaddict <libraryaddict@users.noreply.github.com>
2025-06-24 19:12:06 +1200

    Fix 1.21.6 spigot servers

--- nms/v1_21_R5/src/main/java/me/libraryaddict/disguise/utilities/reflection/v1_21_R5/ReflectionManager.java
+++ nms/v1_21_R5/src/main/java/me/libraryaddict/disguise/utilities/reflection/v1_21_R5/ReflectionManager.java
@@ -113,7 +113,7 @@ public class ReflectionManager extends ReflectionManagerAbstract {
         this.entityCounter = (AtomicInteger) entityCounter.get(null);
 
         // Default is protected method, 1.0F on EntityLiving.class
-        entityDefaultSoundMethod = net.minecraft.world.entity.LivingEntity.class.getDeclaredMethod("fe");
+        entityDefaultSoundMethod = net.minecraft.world.entity.LivingEntity.class.getDeclaredMethod("fk");
         entityDefaultSoundMethod.setAccessible(true);
 
         // Paper did a hard fork, not all features available in paper are available in spigot
@@ -124,7 +124,7 @@ public class ReflectionManager extends ReflectionManagerAbstract {
             // Class exists! This is a paper server, or spigot has updated
             wolfSoundVariantMethods = null;
         } catch (Throwable throwable) {
-            wolfSoundVariantMethods = net.minecraft.world.entity.animal.wolf.Wolf.class.getDeclaredMethod("gM");
+            wolfSoundVariantMethods = net.minecraft.world.entity.animal.wolf.Wolf.class.getDeclaredMethod("gY");
             wolfSoundVariantMethods.setAccessible(true);
             // Failed to load, fall back to nms
         }
