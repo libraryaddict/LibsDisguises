9e4457432f7f15452da76038bfce6b9bfdbe10e5
libraryaddict <libraryaddict@users.noreply.github.com>
2025-08-05 20:26:54 +1200

    Snapshot builds

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
     id("org.hibernate.build.maven-repo-auth") version "3.0.4" apply false
 }
 
-version = "11.0.7"
+version = "11.0.7-SNAPSHOT"
 
 dependencies {
     compileOnly(project(":shared"))
