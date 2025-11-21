38ee0b90c4b839ef506a6f50e91a983dd1d9beeb
libraryaddict <libraryaddict@users.noreply.github.com>
2025-11-22 00:23:15 +1300

    Bump packetevents version

--- plugin/src/main/java/me/libraryaddict/disguise/utilities/updates/PacketEventsUpdater.java
+++ plugin/src/main/java/me/libraryaddict/disguise/utilities/updates/PacketEventsUpdater.java
@@ -64,7 +64,7 @@ public class PacketEventsUpdater {
         // We use a futuristic version if the features we want are in the snapshot builds
         // Actually wait!...
         // If we require snapshot builds, we specify 'SNAPSHOT' in the version string so we don't confuse the users
-        return "2.10.0";
+        return "2.10.1";
     }
 
     /**
@@ -72,7 +72,7 @@ public class PacketEventsUpdater {
      */
     public static Instant getMinimumPacketEventsBuildTimestamp() {
         // As taken from the most recent packetevents compiled jar
-        return Instant.ofEpochMilli(1760902751084L);
+        return Instant.ofEpochMilli(1762279991761L);
     }
 
     public static boolean isPacketEventsOutdated(Instant requiredTime) {
